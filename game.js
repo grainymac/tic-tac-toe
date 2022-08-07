2f7915569eaff169494d62c5bd055f11f28ab4d6
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2022-08-07 10:53:10 -0500

    Finishing up game

--- game.js
+++ game.js
@@ -6,16 +6,17 @@ class Game {
         this.winner = '';
         this.draw = false;
         this.board = [];
-        this.winningConditions = [
-            [1, 2, 3],
-            [4, 5, 6],
-            [7, 8, 9],
-            [1, 5, 9],
-            [2, 5, 8],
-            [3, 5, 7],
-            [1, 4, 7],
-            [3, 6, 9]
-        ];
+        // this.winningConditions = [
+        //     [1, 2, 3],
+        //     [4, 5, 6],
+        //     [7, 8, 9],
+        //     [1, 5, 9],
+        //     [2, 5, 8],
+        //     [3, 5, 7],
+
+        //     [1, 4, 7],
+        //     [3, 6, 9]
+        // ];
     }
 
     playerChange() {
@@ -28,7 +29,24 @@ class Game {
 
     boardPlay() {
         var onTheBoard = this.currentPlayer.board;
-        if (onTheBoard.includes(this.winningConditions[0])) {
+        if (onTheBoard.includes("1") && onTheBoard.includes("2") && onTheBoard.includes("3")) {
+            incrementWins();
+        } else if (onTheBoard.includes("4") && onTheBoard.includes("5") && onTheBoard.includes("6")) {
+            incrementWins();
+        } else if (onTheBoard.includes("7") && onTheBoard.includes("8") && onTheBoard.includes("9")) {
+            incrementWins();
+        } else if (onTheBoard.includes("1") && onTheBoard.includes("4") && onTheBoard.includes("7")) {
+            incrementWins();
+        } else if (onTheBoard.includes("2") && onTheBoard.includes("5") && onTheBoard.includes("8")) {
+            incrementWins();
+        } else if (onTheBoard.includes("3") && onTheBoard.includes("6") && onTheBoard.includes("9")) {
+            incrementWins();
+        } else if (onTheBoard.includes("1") && onTheBoard.includes("5") && onTheBoard.includes("9")) {
+            incrementWins();
+        } else if (onTheBoard.includes("3") && onTheBoard.includes("5") && onTheBoard.includes("7")) {
+            incrementWins();
+        } else if (this.board.length === 9 && !this.winner) {
+            checkDraw();
         }
     }
 }
