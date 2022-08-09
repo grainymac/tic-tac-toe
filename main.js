2c7c66df5018c823d42d31772aa45accf4b74fc1
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2022-08-09 12:43:34 -0500

    Refactor code

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 var game = new Game();
 
+
 // --------------------------------QUERY SELECTORS
 var boxes = document.querySelectorAll('.box');
 var gameBoard = document.querySelector('#game-board');
 var playerTurn = document.querySelector('#player-turn');
 var playerOneWins = document.querySelector('#player-one-wins');
 var playerTwoWins = document.querySelector('#player-two-wins');
+var drawCount = document.querySelector('#draw-count');
 var gameStatus = document.querySelector('#game-status');
 var resetBtn = document.querySelector('#reset-btn');
 
 // --------------------------------EVENT LISTENERS
 resetBtn.addEventListener('click', reset);
-boxes.forEach(function(element) {element.addEventListener('click', chooseSquare)});
+boxes.forEach(function(clickedEvent) {clickedEvent.addEventListener('click', chooseSquare)});
 
 // --------------------------------FUNCTIONS
 function chooseSquare(event) {
@@ -20,11 +22,10 @@ function chooseSquare(event) {
       playerToken(event);
       game.boardPlay();
       switchTurn();
-    
 }
 
 function checkBoardInfo() {
-    if (game.currentPlayer.theBoard.length < 5 && !game.theBoard.includes(event.target) && event.target !== '') {
+    if (game.currentPlayer.theBoard.length < 5) {
       return true;
     } else {
       return false;
@@ -47,14 +48,14 @@ function playerToken(event) {
 function switchTurn() {
     game.playerChange();
     if (game.currentPlayer === game.playerOne && checkGameBoard()) {
-      playerTurn.innerHTML = `<h2>Player 1's Turn!</h2>`;
+      playerTurn.innerHTML = `<h2>Player 1's turn to attack!!</h2>`;
     } else if (game.currentPlayer === game.playerTwo && checkGameBoard()) {
-      playerTurn.innerHTML = `<h2>Player 2's Turn!</h2>`;
+      playerTurn.innerHTML = `<h2>Player 2's turn to attack!!</h2>`;
     }
 }
 
 function checkGameBoard() {
-    if (!game.winner && !game.draw) {
+    if (!game.winner && !game.isDraw) {
       return true;
     } else {
       gameBoard.classList.add('no-click');
@@ -68,27 +69,28 @@ function incrementWins() {
     newTurn();
 }
 
-function checkDraw() {
-    game.draw = true;
+function checkIsDraw() {
+    game.isDraw = true;
+    game.draw.draws += 1;
     newTurn();
 }
 
 function newTurn() {
     if (game.winner === game.playerOne) {
       playerTurn.innerHTML = `<h2>Player One Won!</h2>`;
-      playerOneWins.innerHTML = `Player One Wins:<br>${game.playerOne.wins}</br>`;
+      playerOneWins.innerHTML = `Player One Wins: ${game.playerOne.wins}`;
       game.playerChange();
     } else if (game.winner === game.playerTwo) {
       playerTurn.innerHTML = `<h2>Player Two Won!</h2>`;
-      playerTwoWins.innerHTML = `Player Two Wins:<br>${game.playerTwo.wins}</br>`;
+      playerTwoWins.innerHTML = `Player Two Wins: ${game.playerTwo.wins}`;
       game.playerChange();
-    } else if (game.draw) {
-      playerTurn.innerHTML = `<h2>It's a Draw.</h2>`;
+    } else if (game.isDraw) {
+      playerTurn.innerHTML = `<h2>It's a draw! Play again!</h2>`;
     }
 }
 
 function reset() {
-    game.draw = false;
+    game.isDraw = false;
     game.winner = '';
     gameBoard.classList.remove('no-click');
     switchTurn();
